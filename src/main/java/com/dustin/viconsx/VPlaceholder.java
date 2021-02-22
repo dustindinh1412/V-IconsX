001078ec0cb0fd6bee85fe22cf572fc3204b8448
KienDH <dustindinh.work@gmail.com>
2021-02-22 10:22:22 +0700

    setup class

--- src/main/java/com/dustin/viconsx/VPlaceholder.java
+++ src/main/java/com/dustin/viconsx/VPlaceholder.java
@@ -1,4 +1,26 @@
 package com.dustin.viconsx;
 
-public class VPlaceholder {
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+
+public class VPlaceholder extends PlaceholderExpansion {
+    @Override
+    public String getIdentifier() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer p, String params) {
+        return super.onRequest(p, params);
+    }
 }
